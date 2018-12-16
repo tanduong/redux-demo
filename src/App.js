75bd9f10f8b83f5ddcefba6ec37a2580a4464fc2
Tan Duong <tan.duong@kamereo.vn>
2018-12-16 09:37:50 +0700

    Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 
+const Dashboard = () => <div>Dashboard</div>;
+const Details = ({match}) => <div>Details: {match.params.code}</div>;
 class App extends Component {
   render() {
-    return <div className="App">hello</div>;
+    return (
+      <Router>
+        <Fragment>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/coins/BTC">BTC</Link>
+            </li>
+            <li>
+              <Link to="/coins/ETH">ETH</Link>
+            </li>
+          </ul>
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/coins/:code" component={Details} />
+        </Fragment>
+      </Router>
+    );
   }
 }
 
